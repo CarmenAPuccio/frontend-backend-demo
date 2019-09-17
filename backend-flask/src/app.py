d54f33924986730a56da08a962b74ff3037b1d5e
Carmen Puccio <carmenpuccio@yahoo.com>
2019-09-17 09:35:37 -0400

    Adding the backend service

---
+++ backend-flask/src/app.py
@@ -0,0 +1,34 @@
+import datetime
+import socket
+import time
+import os
+from flask import Flask, request, json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+host_name = socket.gethostname()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Welcome to the Backend Service! Navigate to /backend to see some information...'
+
+@app.route('/backend')
+def backend():
+    format = '%Y-%m-%dT%H:%M:%SZ'
+    unix_epoch = time.time()
+    time_stamp = datetime.datetime.fromtimestamp(unix_epoch)
+
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+
+    json_str = json.dumps({'DateTime': time_stamp.strftime(format), 'Hostname': host_name, 'ClientIP': ip, 'BackendVersion': os.environ['VERSION']})
+    return json_str
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
