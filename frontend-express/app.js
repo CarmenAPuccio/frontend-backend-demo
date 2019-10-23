db861340692a81995686f61cb982ab9d43b0597f
Carmen Puccio <carmenpuccio@yahoo.com>
2019-10-23 15:31:24 -0400

    Changing the logging to pino and new css

--- frontend-express/app.js
+++ frontend-express/app.js
@@ -1,10 +1,7 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
 const dotenv = require('dotenv');
-const logger = require('./config/logger');
+const expressLogger = require('./config/logger');
 
 const indexRouter = require('./routes/index');
 const backendRouter = require('./routes/backend');
@@ -17,32 +14,12 @@ const app = express();
 app.set('./views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(morgan('combined', { stream: logger.stream }));
+app.use(expressLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/backend', backendRouter)
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // add this line to include winston logging
-  logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- frontend-express/config/logger.js
+++ frontend-express/config/logger.js
@@ -1,38 +1,7 @@
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
 
-//const winston = require('winston');
-const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, label, prettyPrint } = format;
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+const expressLogger = expressPino({ logger });
 
-
-// define the custom settings for each transport (file, console)
-const options = {
-  console: {
-    level: 'debug',
-    //format: winston.format.json(),
-    format: combine(
-      timestamp(),
-      format.json()
-    ),
-    handleExceptions: true,
-  },
-};
-
-// instantiate a new Winston Logger with the settings defined above
-//const logger = winston.createLogger({
-const logger = createLogger({
-  transports: [
-    //new winston.transports.Console(options.console)
-    new transports.Console(options.console)
-  ],
-  exitOnError: false, // do not exit on handled exceptions
-});
-
-// create a stream object with a 'write' function that will be used by `morgan`
-logger.stream = {
-  write: function(message, encoding) {
-    // use the 'info' log level so the output will be picked up by both transports (file and console)
-    logger.info(message);
-  },
-};
-
-module.exports = logger;
+module.exports = expressLogger;
